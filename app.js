8c776984e9311ae8f75486050ffc7c105f50d494
Dinesh Wickramasinghe <dineshigdd@gmail.com>
2020-03-04 10:50:37 -0800

    Add validation, auth, tweets

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require('passport');
+require('./config/passport')(passport);
 
 const db = require('./config/keys').mongoURI;
-mongoose.connect(db,{ useNewUrlParser:true})
-        .then(()=>console.log("connected to db"))
-        .catch( err + console.log(err));
+mongoose.connect(db,{ useNewUrlParser:true })
+        .then(() => console.log("connected to db"))
+        .catch( err => console.log(err));
 
-app.get("/", (req,res) => res.send("Hello world"));
+app.use(passport.initialize());
+
+
+//setup some middleware for body parser:
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);   //Tell Express to use your newly imported routes
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
