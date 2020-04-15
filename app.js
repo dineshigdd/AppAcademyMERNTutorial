bbcf72b6a39e4aee026bfdc511135a70678dd6b0
Dinesh Wickramasinghe <dineshigdd@gmail.com>
2020-04-15 03:25:56 -0700

    changes add to app.js and package.json in server

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const passport = require('passport');
 require('./config/passport')(passport);
+const path = require('path');
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db,{ useNewUrlParser:true })
@@ -24,3 +25,11 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//tell our server to load the static build folder in production:
+if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('frontend/build'));
+        app.get('/', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        })
+}
